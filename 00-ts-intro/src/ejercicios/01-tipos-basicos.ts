c6c9af18d6b7d8e96db6b299836286eebc568f5a
David Cascos <davidcascosgimeno@gmail.com>
2021-11-10 20:54:07 +0100

    Tipos básicos y conceptos generales

---
+++ 00-ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,9 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+let hp: number | string = 95; //al indicarle 2 tipos se hace que la variable pueda ser los 2
+let estaVivo: boolean = true;
+
+console.log(nombre, hp);
