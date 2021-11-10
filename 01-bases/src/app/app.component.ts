1ce2ad9f1463275a5bb46f8abfef1b16d149772c
David Cascos <davidcascosgimeno@gmail.com>
2021-11-10 21:24:10 +0100

    Contador App: (Tarea) utilizar variable para incrementar y decrementar el contador

--- 01-bases/src/app/app.component.ts
+++ 01-bases/src/app/app.component.ts
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public titulo: string = 'Contador App';
-  public numero:number=10;
+  public numero: number = 10;
+  public base: number = 5;
+
 
   acumular(valor: number) {
     this.numero += valor;
-}
+  }
 }
